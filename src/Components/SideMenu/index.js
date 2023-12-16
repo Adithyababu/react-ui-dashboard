9bc47619d63be9fd49b7ba268641aeb07ce27fb1
Adithyababu <adithyababu865@gmail.com>
2023-12-16 14:24:14 +0530

    final code

--- src/Components/SideMenu/index.js
+++ src/Components/SideMenu/index.js
@@ -1,10 +1,13 @@
-import {Menu} from "antd";
+import {Menu,Image, Typography} from "antd";
 import {AiTwotoneAppstore} from "react-icons/ai";
 import {FaQuestion} from "react-icons/fa";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function SideMenu() {
     return (<div className="SideMenu">
-        <Menu items={[
+    <Image style={{paddingTop:'20px',paddingLeft:"40px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png" width="100px" ></Image>
+        <Typography.Title level={5} style={{paddingLeft:"30px"}}>Ram Mohan ></Typography.Title> 
+        <Typography.Title style={{paddingLeft:"10px",fontSize:"13px",paddingRight:"20px"}}>rammohan2@gmail.com</Typography.Title>
+<Menu className="menu" items={[
             {
             label:"Dashboard",
             icon:<AiTwotoneAppstore/>,
